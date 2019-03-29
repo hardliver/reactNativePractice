d37dfeff8c4cf4d79f43b27df9c4645715da0d76
Hardliver <yelionel26@gmail.com>
2019-03-29 21:48:09 +0800

    React Navigation & Main Screen

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import store from './store';
+import MainScreen from './screens/MainScreen';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
+    const MainNavigator = createAppContainer(
+      createStackNavigator({
+        Main: { screen: MainScreen }
+      })
+    );
+
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
